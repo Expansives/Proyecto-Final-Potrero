3dce822554c17d30b0724e3a28c9baaf2c314125
Expansives <58773607+Expansives@users.noreply.github.com>
2021-10-10 21:26:18 -0300

    Nav slide

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
+
 const NavSlide = () => {
     const burger = document.querySelector('.nav__burger');
     const nav = document.querySelector('.nav__ul');
-
+    let menuOpen = false;
 
     //Toogle Nav
     burger.addEventListener('click', () =>{
+        if(!menuOpen) {
+            burger.classList.add('open');
+            menuOpen = true;
+        }   else {
+            burger.classList.remove('open');
+            menuOpen = false;
+        }
+
         nav.classList.toggle('nav-active');
     
     });
